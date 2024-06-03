4c8ed51ac078a8ea35df1adf8d6c07df648f43d9
maxim12313 <maximkim16@gmail.com>
2024-06-03 13:20:35 -0700

    rewriting

--- app/components/TextArea.js
+++ app/components/TextArea.js
@@ -6,9 +6,9 @@ export function TextArea() {
   const [words, setWords] = useState([]);
   const [correct, setCorrect] = useState([]);
 
-
   useEffect(() => { 
-
+    //must always maintain a minimum of 100 words
+    //must always be 100 unfinished words
   });
 
   const wordComponents = words.map((word, i) => {
@@ -17,7 +17,7 @@ export function TextArea() {
 
   return (
     <div className="w-full h-full flex flex-col items-center justify-center helper">
-      <div className="w-full h-60 flex-row p-5 helper">
+      <div className="max-w-[2000px] w-full h-60 flex-row p-5 helper">
         { wordComponents }
       </div>
       <button className="mt-5">
