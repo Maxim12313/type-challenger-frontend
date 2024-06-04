fca180e0bddded6de078b6944b8944cd44ca2734
maxim12313 <maximkim16@gmail.com>
2024-06-04 00:21:49 -0700

    deleting

--- app/components/TextArea.js
+++ app/components/TextArea.js
@@ -90,7 +90,6 @@ export default function TextArea() {
 
     };
 
-
     window.addEventListener("keydown", keyDown);
 
     return () => {
--- app/jian/singleplayer/page.js
+++
@@ -1,9 +0,0 @@
-import {Page} from '@/app/components2/singleplayer-components'
-
-export default function Jian() {
-  return (
-    <div>
-      <Page/>
-    </div>
-  );
-}
