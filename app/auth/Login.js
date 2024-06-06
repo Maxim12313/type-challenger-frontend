04147c97bc9692923b1dfe1dbdbd982f3d78b4f5
Jian Hong Li <lijh0901@gmail.com>
2024-06-06 19:53:17 -0400

    Added some spaces

--- app/auth/Login.js
+++ app/auth/Login.js
@@ -7,6 +7,7 @@ export default function Login() {
     <div className="w-full h-full flex flex-col items-center justify-center helper">
       <div className="border-b-2 font-bold text-5xl pb-3">LOGIN</div>
       <InputForm method="login"/>
+      <br/><br/><br/>
       <p className='py-3 text-xl'> Or Sign Up Here </p>
       <Link href="/multiplayer/SignUp" className='font-bold'> SIGN UP </Link>
     </div>
--- app/auth/SignUp.js
+++ app/auth/SignUp.js
@@ -7,6 +7,7 @@ export default function Login() {
     <div className="w-full h-full flex flex-col items-center justify-center helper">
       <div className="border-b-2 font-bold text-5xl pb-3">SIGNUP</div>
       <InputForm method="signup"/>
+      <br/><br/><br/>
       <p className='py-3 text-xl'> Or Login Here </p>
       <Link href="/multiplayer" className='font-bold'> LOGIN </Link>
     </div>
